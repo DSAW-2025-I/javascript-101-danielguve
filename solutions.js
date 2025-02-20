16d61e961aa195da9511784d6345b99b97a1442a
danielguve <danielguve@unisabana.edu.co>
2025-02-19 22:29:37 -0500

    Update solutions.js

--- solutions.js
+++ solutions.js
@@ -1,54 +1,71 @@
-// Sum of Two Numbers
 function sum(a, b) {
-    return a + b;
+  if (typeof a !== "number" || typeof b !== "number") {
+
+    console.log("Malos datos");
+    return;
+  }
+  return a + b;
 }
 
 // Factorial of a Number
 function factorial(n) {
-if (n === 0) return 1;
+  if (typeof n !== "number" || !Number.isInteger(n) || n < 0) {
+    console.log("Malos datos");
+    return;
+  }
+  if (n === 0) return 1; 
+  let factorial = 1;
   
- 
-  if (n < 0) return "Error";
-
-  return n * factorial(n - 1);
-
+  }return n * factorial(n - 1);
+  
 }
 
 // Find the Largest Number
 function findLargest(arr) {
-  
- let largest = arr[0];  
-  
-  for (let i = 1; i < arr.length; i++) {
-  
-    if (arr[i] > largest) {
-      largest = arr[i];
-    }
+   if (typeof n !== 'number') {
+    console.log("Malos datos")
+  }
+ if (n <= 1) return false;
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) return false;  
   }
 
-  return largest;
-  }
-
+  return true;
+}
 
 // Count Vowels in a String
 function countVowels(str) {
-  return str.split('').filter(char => 'aeiouAEIOU'.includes(char)).length;
-}
-function isPrime(n) {
- if (n <= 1) return false;
-  
-
-  for (let i = 2; i <= Math.sqrt(n); i++) {
-    if (n % i === 0) return false;  
+  if (typeof str !== "string") {
+    console.log("Malos datos");
+    return;
   }
+ for (let i = 1; i < arr.length; i++) {  
+    if (arr[i] > largest) {  
+      largest = arr[i]; 
+    }
+}
+
+// Check if a Number is Prime
+function isPrime(n) {
+  if (typeof n !== "number" || !Number.isInteger(n) || n < 2) {
+    console.log("malos datos");
+    return;
+  }
+   if (n <= 1) return false;
+  
+  for (let i = 2; i <= n / 2; i++) {
+    if (n % i === 0) return false; 
+  }
+  
   
   return true;
 }
 
+// Exportar funciones para pruebas en Node.js
 module.exports = {
   sum,
   factorial,
   findLargest,
   countVowels,
-  isPrime,
+  isPrime,
 };
