c8ec103ad06b714ada3ca236c835650d35ad4bd5
danielguve <danielguve@unisabana.edu.co>
2025-02-19 22:02:46 -0500

    Update solutions.js

--- solutions.js
+++ solutions.js
@@ -1,12 +1,15 @@
 // Sum of Two Numbers
 function sum(a, b) {
+  console.log("Datos validos");
 return a + b;
 }
 
 // Factorial of a Number
 function factorial(n) {
+
   if (n === 0) return 1;
   if (n < 0) return "Error"; 
+  console.log("Datos validos");
   return n * factorial(n - 1);
 }
 
@@ -15,7 +18,8 @@ function findLargest(arr) {
   
   let largest = arr[0];  
   for (let i = 1; i < arr.length; i++) {  
-    if (arr[i] > largest) {  
+    if (arr[i] > largest) { 
+      console.log("Datos validos");
       largest = arr[i];  
     }
   }
@@ -25,6 +29,7 @@ function findLargest(arr) {
 
 // Count Vowels in a String
 function countVowels(str) {
+  console.log("Datos validos");
   return str.split('').filter(char => 'aeiouAEIOU'.includes(char)).length;
 }
 
@@ -32,6 +37,7 @@ function countVowels(str) {
 function isPrime(n) {
  if (n <= 1) return false;
   for (let i = 2; i <= Math.sqrt(n); i++) {
+    console.log("Datos validos");
     if (n % i === 0) return false;
   }
   return true;
